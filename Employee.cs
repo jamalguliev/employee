003b6c984871dba584ceb5a268c1ad1f2f73cbd1
jamalguliev <94452049+jamalguliev@users.noreply.github.com>
2021-12-03 15:14:53 +0400

    first commit

---
+++ Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Employee
+    {
+
+        public string name;
+        public string surname;
+        public string position;
+        public int salary;
+
+
+
+        public void Sum()
+        {
+            Console.WriteLine(salary *30);
+        }
+
+    
+
+        
+    }
+
+}
+        
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.name = "jamal";
+            emp1.surname = "guliyev";
+          
+            emp1.position = "manager";
+            emp1.salary  = 50;
+
+            Console.WriteLine($"{emp1.name}, {emp1.surname}, {emp1.position} ");
+            emp1.Sum();
+
+
+
+
+
+
+        }
+    }    
+}
